c5078fa662770f6b1619be08d21e41c14508cf38
heeneth <heeneth123@gmail.com>
2024-03-18 11:40:23 +0530

    added cart list and methodes to remove from cart and add to cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { response } from 'express';
+import { Router } from '@angular/router'; // Import Router module
 
 @Component({
   selector: 'app-cart',
@@ -9,7 +11,7 @@ import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 export class CartComponent {
   data: any;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
     this.cartData(); // Invoke cartData() method in the constructor
   }
 
@@ -23,5 +25,24 @@ export class CartComponent {
       this.data = response;
       console.log('Cart Data:', this.data); // Log the object directly
     });
+  }
+
+  //logic to remove cart items for cartID
+  removeFormCartByCartID(cartId: number) {
+    const url = 'http://localhost:8081/removeFromCartByCartID/' + cartId;
+
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    this.http.post(url, {}, { headers, responseType: 'text' }).subscribe(
+      (response) => {
+        console.log(response);
+        this.router.navigate(['cart']); // Navigation after successful request
+        window.location.reload();
+      },
+      (error) => {
+        console.error('Error removing item from cart:', error);
+        alert('Error removing item from cart. Please try again later.'); // Example of displaying an error message
+      }
+    );
   }
 }
