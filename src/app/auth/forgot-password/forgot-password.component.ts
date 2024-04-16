3a2d9e49218d41d80386c4736c30750003c1f8f2
heeneth <heeneth123@gmail.com>
2024-04-16 22:45:57 +0530

    added loding svg function in forgot password component

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.css'
+  styleUrls: ['./forgot-password.component.css'], // Corrected property name
 })
 export class ForgotPasswordComponent {
+  email: string = ''; // Declare email variable
+  loadingSvg: boolean = false;
 
+  constructor(private http: HttpClient) {}
+
+  sendOTPtoUser() {
+    const url = 'http://localhost:8081/forgotPassword';
+    this.loadingSvg = !this.loadingSvg;
+    this.http.post<any>(url, { email: this.email }).subscribe((response) => {
+      console.log(response);
+      if (response.message == 'OTP send') {
+        this.loadingSvg = !this.loadingSvg;
+      }
+    });
+  }
 }
