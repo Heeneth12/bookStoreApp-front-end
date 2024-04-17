bc27e81c63969b98e5556e988259cafbede32d11
heeneth <heeneth123@gmail.com>
2024-04-17 21:40:44 +0530

    added endpoints to the my orders list

--- src/app/my-orders-list/my-orders-list.component.ts
+++ src/app/my-orders-list/my-orders-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-my-orders-list',
   templateUrl: './my-orders-list.component.html',
-  styleUrl: './my-orders-list.component.css'
+  styleUrls: ['./my-orders-list.component.css'],
 })
-export class MyOrdersListComponent {
+export class MyOrdersListComponent implements OnInit {
+  orderData: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getYourOrders();
+  }
+
+  getYourOrders() {
+    const url = 'http://localhost:8081/getAllCartItems';
+    const token = localStorage.getItem('jwtToken');
+    const headers = new HttpHeaders().set('token', `${token}`);
+    this.http.get<any[]>(url, { headers }).subscribe((response) => {
+      console.log(response);
+      // Filter the array based on the 'ordered' property being true
+      this.orderData = response.filter((item) => item.ordered === true);
+      console.log(this.orderData);
+    });
+  }
 }
